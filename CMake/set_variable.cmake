d52443d69b12442c4aae3484a9d072a6ee65c4cc
shensunbo <shensunbo@foxmail.com>
2024-09-13 22:16:30 +0800

    compile imgui to so lib

--- CMake/set_variable.cmake
+++ CMake/set_variable.cmake
@@ -2,17 +2,16 @@ set(COMMON_INC_DIR
     ${ROOT_DIR}/include
     ${ROOT_DIR}/dependency
     ${ROOT_DIR}/dependency/stb
-    ${ROOT_DIR}/dependency/imgui
+    ${ROOT_DIR}/dependency/imgui/include
     ${ROOT_DIR}/dependency/assimp/include
     ${ROOT_DIR}/dependency/glad/include
 )
 
 set(COMMON_LIB_DIR 
     ${ROOT_DIR}/dependency/assimp/lib
+    ${ROOT_DIR}/dependency/imgui/lib
 )
 
-file(GLOB IMGUI_SRC_FILES ${ROOT_DIR}/dependency/imgui/*.cpp)
 set(COMMON_DEP_SRC_FILES 
-    ${IMGUI_SRC_FILES}
     ${ROOT_DIR}/dependency/glad/src/glad.c
 )
